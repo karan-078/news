712b99e4da62913a9b609f91efc64924900a85e9
karan <karanbisht117@gmail.com>
2022-08-30 17:41:17 +0530

    intial commit

--- src/Tour/Tour.js
+++ src/Tour/Tour.js
@@ -1,58 +1,56 @@
-import React, { useState } from 'react'
-import './tour.css';
-import data from './data'
+import React, { useState } from "react";
+import "./tour.css";
+import data from "./data";
 const Tour = () => {
-    const [tour , settour] = useState(data)
-    const [redMore , setredMore] = useState(false)
-    
-    const fliteritem = (id)=>{
-        const newTour = tour.filter((tour)=>tour.id!==id);
-        settour(newTour)
-    }
+  const [tour, settour] = useState(data);
+  const [redMore, setredMore] = useState(false);
 
-    const refresh = ()=>{
-        if(tour.length=0){
-          const refreshtour= tour.length-1;
-          return refreshtour;
-        }
-       
-    }
+  const fliteritem = (id) => {
+    const newTour = tour.filter((tour) => tour.id !== id);
+    settour(newTour);
+  };
+
   return (
-  <div className="main-container">
-   {
-  tour.map((info)=>{
-    const {id , img , descruptioon, price,about} = info
+    <div className="main-container">
+      {tour.map((info) => {
+        const { id, img, descruptioon, price, about } = info;
 
-    return (
-        <div className="infocontainer" key={id}>
-         <div className="image">
-            <img src={img} alt={descruptioon} />
-         </div>
+        return (
+          <div className="infocontainer" key={id}>
+            <div className="image">
+              <img src={img} alt={descruptioon} />
+            </div>
 
-         <div className="price">
-           <h2>{descruptioon}</h2>
-           <p>{price}</p>
-         </div>
+            <div className="price">
+              <h2>{descruptioon}</h2>
+              <p>{price}</p>
+            </div>
 
-         <div className="about">
-            <p>{redMore?about:`${about.substring(0,100)}.......`}
-            
-            <button onClick={()=>{setredMore(!redMore)}}> {redMore?'...show less': 'read more'}</button>
-            </p>
-         </div>
-         
-         <div className='btn'>
-          <button className='fliteritem' onClick={()=>fliteritem(id)}>notintrsted</button>
+            <div className="about">
+              <p>
+                {redMore ? about : `${about.substring(0, 100)}.......`}
+
+                <button
+                  onClick={() => {
+                    setredMore(!redMore);
+                  }}
+                >
+                  {" "}
+                  {redMore ? "...show less" : "read more"}
+                </button>
+              </p>
+            </div>
+
+            <div className="btn">
+              <button className="fliteritem" onClick={() => fliteritem(id)}>
+                notintrsted
+              </button>
+            </div>
           </div>
-        </div>
-    );
-  })
-   }
-   <div className="refresh">
-   <button onClick={settour(refresh)}>refresh</button>
-   </div>
-  </div>
-  )
-}
+        );
+      })}
+    </div>
+  );
+};
 
-export default Tour
+export default Tour;
---
+++ src/reviwes/data.js
@@ -0,0 +1,42 @@
+const data = [
+  {
+    id: 1,
+    name: "karan",
+    job: "webdevlper",
+    img: "https://images.pexels.com/photos/1270076/pexels-photo-1270076.jpeg?auto=compress&cs=tinysrgb&w=600",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa temporibus cumque voluptatum laudantium praesentium quos dolore id maxime dolor doloremque",
+  },
+
+  {
+    id: 2,
+    name: "saurabh",
+    job: "webdevlper",
+    img: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=600",
+    text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat impedit cum eveniet facere minima officia! Corrupti, eveniet? Odio, nobis dolorem. ",
+  },
+
+  {
+    id: 3,
+    name: "Lalit",
+    job: "webdevlper",
+    img: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=600",
+    text: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo labore pariatur, iste dolor consequuntur reiciendis sunt quis, modi a tempora incidunt similique. ",
+  },
+
+  {
+    id: 4,
+    name: "sonu",
+    job: "webdevlper",
+    img: "https://images.pexels.com/photos/1121796/pexels-photo-1121796.jpeg?auto=compress&cs=tinysrgb&w=600",
+    text: " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto est a eos enim quia ut ab commodi nulla, aliquid ea similique facere.",
+  },
+  {
+    id: 5,
+    name: "tanu",
+    job: "webdevlper",
+    img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=600",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, placeat. Neque, harum. Laudantium aliquam quas beatae magni fuga voluptate voluptatum assumenda animi?",
+  },
+];
+
+export default data;
